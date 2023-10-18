243ce9a80065368d6090cec5988be61c862f98b7
selmahabota <selma.habota@gmail.com>
2023-10-18 21:00:50 +0200

    Promjena uslova za izlaz iz unosa

--- src/Main.java
+++ src/Main.java
@@ -6,37 +6,44 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    public static void unosStudenta(List<Student> listaStudenata, List<Student> listaStudenataPolozili){
-        String unos;
+    public static void unosStudenta(List<Student> listaStudenata, List<Student> listaStudenataPolozili) {
+
+        String ime;
+        String prezime;
+        String ocjena;
         Scanner scanner = new Scanner(System.in);
-        do {
+        while (true) {
             System.out.println("Unesite ime studenta: ");
-            String ime = scanner.next();
+            ime = scanner.next();
+            if (ime.equals("quit")) break;
             System.out.println("Unesite prezime studenta: ");
-            String prezime = scanner.next();
+            prezime = scanner.next();
+            if (prezime.equals("quit")) break;
             System.out.println("Unesite ocjenu studenta: ");
-            int ocjena = scanner.nextInt();
-            System.out.println("Unos novog studenta? Y za unos, quit za izlaz iz unosa");
-            unos=scanner.next();
-            Student studenti= new Student(ime, prezime, ocjena);
+            ocjena = scanner.next();
+            if (ocjena.equals("quit")) break;
+            Student studenti = new Student(ime, prezime, Integer.parseInt(ocjena));
             listaStudenata.add(studenti);
-            if(ocjena>5) listaStudenataPolozili.add(studenti);
-        } while (!unos.equals("quit"));
+            if (Integer.parseInt(ocjena) > 5) listaStudenataPolozili.add(studenti);
+        }
     }
+
     public static void main(String[] args) {
 
         List<Student> listaStudenata = new ArrayList<>();
-        List<Student> listaStudenataPolozili=new ArrayList<>();
+        List<Student> listaStudenataPolozili = new ArrayList<>();
         unosStudenta(listaStudenata, listaStudenataPolozili);
         System.out.println("Svi uneseni studenti:");
-        for (Student s:listaStudenata)
-        {
-            System.out.println(s);
-        }
-        System.out.println("Studenti koji su polozili: ");
-        for (Student s:listaStudenataPolozili)
-        {
-            System.out.println(s);
+        if (listaStudenata.isEmpty()) {
+            System.out.println("Lista je prazna");
+        } else {
+            for (Student s : listaStudenata) {
+                System.out.println(s);
+            }
+            System.out.println("Studenti koji su polozili: ");
+            for (Student s : listaStudenataPolozili) {
+                System.out.println(s);
+            }
         }
     }
 }
